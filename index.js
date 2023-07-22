da940d077ffeca9f8432dc823bb735023d93e303
AbhimanyuSingh2005 <112697432+AbhimanyuSingh2005@users.noreply.github.com>
2023-07-23 01:40:58 +0530

    Add files via upload
    This commit contain complete project with working carousel.

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 
 $(document).scroll(function () {
     var position = window.scrollY;
-    if(position>10){
+    if (position > 10) {
         $(".navbar").addClass("afternav");
-    }else{
+    } else {
         $(".navbar").removeClass("afternav");
     }
+})
+
+
+
+const buttons = document.querySelectorAll(".carousel-btn");
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const direction = button.getAttribute("data-btn") === "next" ? 1 : -1;
+        const slides = document.querySelectorAll(".project");
+        const activeSlide = document.querySelector("[data-active]");
+        const rightSlide = document.querySelector("[data-right]");
+        const leftSlide = document.querySelector("[data-left]");
+        const slideArray = [...slides];
+        const indexActive =slideArray.indexOf(activeSlide)+direction;
+        const indexRight =slideArray.indexOf(rightSlide)+direction;
+        const indexLeft =slideArray.indexOf(leftSlide)+direction;
+        const index = [indexLeft ,indexActive ,indexRight];
+        index.forEach((i,j) =>{
+            if (i < 0 ){
+                index[j] = slideArray.length-1;
+            }else if(i>=slideArray.length){
+                index[j]=0;
+            }
+        })
+
+        slideArray[index[1]].dataset.active = true ;
+        slideArray[index[2]].dataset.right = true ;
+        slideArray[index[0]].dataset.left = true ;
+        delete slideArray[indexActive-direction].dataset.active;
+        delete slideArray[indexRight-direction].dataset.right;
+        delete slideArray[indexLeft-direction].dataset.left;
+        
+    })
 })
